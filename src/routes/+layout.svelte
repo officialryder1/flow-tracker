4f3aa8ee4f3d2d423b726a075614739dafeb634a
officialryder1~ <kennethvic07@gmail.com>
2026-02-12 15:45:12 +0100

    remv favicon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
 	import './layout.css';
-	import favicon from '$lib/assets/favicon.svg';
 	import { Toaster } from 'svelte-sonner';
 	import { ModeWatcher } from "mode-watcher";
 	import { onMount } from 'svelte';
@@ -42,7 +41,6 @@
 	}
 </script>
 
-<svelte:head><link rel="icon" href={favicon} /></svelte:head>
 
 <ModeWatcher />
 {#if mounted}
