c8ad9cd6024a1e260dc20257c86d38c4c4c46c94
officialryder1~ <kennethvic07@gmail.com>
2026-02-17 13:56:38 +0100

    added updater for service worker

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -15,6 +15,7 @@
 
 	onMount(() => {
 		mounted = true
+		detectServiceWorkerUpdate();
 	})
 
 	let updateAvailable = $state(false);
@@ -35,6 +36,7 @@
 			});
     	});
 	}
+
 
 	function applyUpdate() {
 		registration?.waiting?.postMessage({ type: 'SKIP_WAITING' });
